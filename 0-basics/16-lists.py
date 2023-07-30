3ac6d6e734cf97b1672e9f4d81ed97e2bdf5459e
NokTNL <noklamtsz@gmail.com>
2023-07-31 00:53:18 +0100

    Tuples notes

--- 0-basics/16-lists.py
+++ 0-basics/16-lists.py
@@ -1,7 +1,8 @@
-import random
-
 # LISTS are basically arrays, defined with `[]`
 food = ['pizza', 'burger', 'pasta', 'sushi']
+print(type(food)) # <class 'list'>
+# Check length with len()
+print(len(food)) # 4
 # Item access using []
 print(food[1]) # burger
 print(food[-1]) # sushi; NEGATIVE index count from FIRST element as zero
@@ -12,11 +13,12 @@ print(food[1]) # ramen
 # Checking if a value is in a list
 print('pizza' in food) # True
 print('salad' in food) # False
-# To get a random item from an array:
-my_choice = random.choice(food)
+# list.index(): returns the index of the FIRST matching item
+print(food.index('pasta')) # 2
+# print(food.index('IIUI')) # ValueError: 'IIUI' is not in list
 
 
-# Mutating list methods
+# List manipulation
 boys = ['Tom', 'Paul', 'Julian']
 
 ### append(): add ONE item to the end of the array 
@@ -31,6 +33,9 @@ print(boys) # ['Tom', 'Paul', 'Julian', 'John', 'Karl', 'Sam']
 boys += ['HA']
 print(boys) # ['Tom', 'Paul', 'Julian', 'John', 'Karl', 'Sam', 'HA']
 # !!! If you want to extend with a single string array (e.g. 'Sam') but forget the `[]`, the string will be added AS A LIST --> 'S', 'a', 'm'
+
+### Joining arrays with `+`
+print([1, 'A'] + [2, 'B']) # [1, 'A', 2, 'B']
 
 ### remove(): remove an item from the array that matches the value
 boys.remove('HA')
@@ -47,7 +52,7 @@ print(boys) # ['Tom', 'Paul', 'INSERTED', 'INSERTED2', 'INSERTED3', 'Julian', 'J
 
 ### sort()
 # It sorts using "<", so an all-string array will be sorted alphabetically, and an number array numerically
-# Sorting a mixed str and numbers in an array will give error
+# Sorting a mixed str and number array will give error
 numbers = [1, 5, 3, 2, 4]
 numbers.sort()
 print(numbers) # [1, 2, 3, 4, 5]
